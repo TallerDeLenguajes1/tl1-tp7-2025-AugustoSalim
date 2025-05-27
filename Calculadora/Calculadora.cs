e413ace51f0101a94616beb7e8ff825d24d4a3da
lusissy <lourdessissy@gmail.com>
2025-05-27 15:37:02 -0300

    Calculadora lista, falta el menu en program

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,35 @@
+namespace EspacioCalculadora;
+//campos con minuscula - Metodo y Propiedades con Mayuscula la primera
+public class Calculadora
+{
+    private double resultado;//por defecto se inicializa en 0
+
+    public double Resultado { get => resultado; }
+
+    public void Sumar(double termino)
+    {
+        resultado+=termino;
+    }
+    public void Restar(double termino)
+    {
+        resultado-=termino;
+    }
+    public void Dividir(double termino)
+    {
+        if (termino != 0)
+        {
+            resultado/=termino;
+        } else
+        {
+            Console.WriteLine("No valida la division por 0");
+        }
+    }
+    public void Multiplicar(double termino)
+    {
+        resultado*=termino;
+    }
+    public void Limpiar()
+    {
+        resultado = 0;
+    }
+}
--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EspacioCalculadora;
+
+//Lo declaro y le hago new antes de usarlo porque sino el objeto no existe, similar a lo que pasaba con puntero
+Calculadora calculadora = new Calculadora();
+
+Console.WriteLine(calculadora.Resultado.ToString());
+calculadora.Sumar(10);
+Console.WriteLine(calculadora.Resultado.ToString());
+
