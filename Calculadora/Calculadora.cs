95ffc52b5ab4bac9e6cba1ef2a6df0fc50dd7c19
AugustoSalim <augustosalim98@gmail.com>
2025-06-28 17:39:52 -0300

    cambios para probar en gitlens

--- Calculadora/Calculadora.cs
+++ Calculadora/Calculadora.cs
@@ -1,5 +1,6 @@
 namespace EspacioCalculadora;
 //campos con minuscula - Metodo y Propiedades con Mayuscula la primera
+//Linea para probar gitlens
 public class Calculadora
 {
     private double resultado;//por defecto se inicializa en 0
@@ -8,25 +9,26 @@ public class Calculadora
 
     public void Sumar(double termino)
     {
-        resultado+=termino;
+        resultado += termino;
     }
     public void Restar(double termino)
     {
-        resultado-=termino;
+        resultado -= termino;
     }
     public void Dividir(double termino)
     {
         if (termino != 0)
         {
-            resultado/=termino;
-        } else
+            resultado /= termino;
+        }
+        else
         {
             Console.WriteLine("No valida la division por 0");
         }
     }
     public void Multiplicar(double termino)
     {
-        resultado*=termino;
+        resultado *= termino;
     }
     public void Limpiar()
     {
